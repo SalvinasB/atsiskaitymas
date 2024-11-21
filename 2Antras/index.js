3f13f03d4913b3c94916002993af0b093f4aeec7
SalvinasB <salvinas.beliajevskis@stud.kitm.lt>
2024-11-21 12:41:46 +0200

    Pirmas + Antras

---
+++ 2Antras/index.js
@@ -0,0 +1,8 @@
+const vaisiai = ["obuolys", "bananas", "vynuoges", "kriause", "apelsinas"];
+
+function kiek(naujas) {
+  return naujas.length;
+}
+
+const num = kiek(vaisiai);
+console.log(`Yra ${num} vaisiu`);
