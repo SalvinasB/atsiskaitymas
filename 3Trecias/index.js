9b21272af101e8ac3a874c036614c4ba3bb16e24
SalvinasB <salvinas.beliajevskis@stud.kitm.lt>
2024-11-21 12:51:47 +0200

    + Trecias

--- 3Trecias/index.js
+++ 3Trecias/index.js
@@ -1,1 +1,11 @@
-const knyga = {};
+const knyga = {
+  pav: "The Art of Star Wars: The Mandalorian",
+  autorius: "Phil Szostak",
+  puslapiu: 256,
+};
+
+function apibudinimas() {
+  return `Knyga "${knyga.pav}", autorius ${knyga.autorius}, puslapiu ${knyga.puslapiu}`;
+}
+
+console.log(apibudinimas);
